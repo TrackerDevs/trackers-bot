3fe3361163bce7c15b94f9fee4510b4de55e4137
Hamziniii <hamzagaziuddin04@gmail.com>
2021-09-13 14:48:52 -0500

    Added replyOrFollowup util function

--- lib/util.ts
+++ lib/util.ts
@@ -1,2 +1,8 @@
+import { CommandInteraction, MessageComponentInteraction } from "discord.js"
+
 /** Returns a promise that waits for (ms) milliseconds before resolving */
 export const sleep = async (ms: number): Promise<void> => new Promise(resolve => setTimeout(resolve, ms))
+
+/** Checks if an interaction has already been replied. If so, return followup function; else, return reply function. Use this if you are going to be using a command within another command */
+export const replyOrFollowup = (interaction: CommandInteraction | MessageComponentInteraction) => interaction?.replied ? 'followUp' : 'reply'
+// I cant just return interaction.followUp or reply for some reason. Idk why tho
