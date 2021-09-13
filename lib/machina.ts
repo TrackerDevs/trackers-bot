413aee7f7011be2dfb9044b3f25166bca95bdd4e
Hamziniii <hamzagaziuddin04@gmail.com>
2021-09-13 15:31:39 -0500

    Added extra interaction handler

--- lib/machina.ts
+++ lib/machina.ts
@@ -1,7 +1,7 @@
 import 'discord.js'
 import { REST } from '@discordjs/rest'
 import { Routes } from 'discord-api-types/v9'
-import { Client, Collection, CommandInteraction, Intents, Interaction } from 'discord.js'
+import { ButtonInteraction, Client, Collection, CommandInteraction, ContextMenuInteraction, Intents, Interaction, MessageComponentInteraction, SelectMenuInteraction } from 'discord.js'
 import * as fs from 'fs'
 import { SlashCommandBuilder } from '@discordjs/builders'
 import { sleep } from './util'
@@ -31,21 +31,62 @@ export class Machina {
 
     /** Starts the bot */
     start() {
+        if(!this.client.commands) {
+            this.client.commands = new Collection();
+            for (const file of fs.readdirSync(path.join(__dirname, '../commands')).filter(file => file.endsWith('.js') || file.endsWith('.ts'))) {
+                let _command = require(`../commands/${file}`)
+                const name = Object.getOwnPropertyNames(_command)[1]
+                let command: Machi = _command[name]
+                if(!command.inDev && command.data && command.execute) {
+                    if(command.data.name == undefined)
+                        command.data.setName(name)
+                    console.log(command.data.name, "has been added!")
+                    this.client.commands.set(command.data.name, command)
+                }
+            }
+        }
+
         this.client.once('ready', () => {
             console.log('Bot Online!')
         })
         this.client.on('interactionCreate', async interaction => {
-            if (!interaction.isCommand()) return
-        
-            const command = this.client.commands.get(interaction.commandName)
-        
-            if (!command) return
-        
-            try {
-                await command.execute(interaction)
-            } catch (error) {
-                console.error(error)
-                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+            if (interaction.isCommand()) {
+                const command = this.client.commands.get(interaction.commandName)
+            
+                if (!command) return
+            
+                try {
+                    await command.execute(interaction, this)
+                } catch (error) {
+                    console.error(error)
+                    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+                }
+            } else if(interaction.isMessageComponent()) {
+                // let type: 'button' | 'contextMenu' | 'selectMenu'
+                let [commandName, interactionName] = interaction.customId.split('.')
+                let command 
+
+                const error = () => interaction.reply({ content: 'Uh Oh! The devs made a mistake while creating this command.', ephemeral: true})
+
+                if(interaction.isButton()) {
+                    command = this.client.commands.get(commandName)['button']
+                    if(command[interactionName])
+                        command[interactionName](interaction, this)
+                    else 
+                        await error()
+                } else if(interaction.isContextMenu()) {
+                    command = this.client.commands.get(commandName)['contextMenu']
+                    if(command[interactionName])
+                        command[interactionName](interaction, this)
+                    else 
+                        await error()
+                } else if(interaction.isSelectMenu()) {
+                    command = this.client.commands.get(commandName)['selectMenu']
+                    if(command[interactionName])
+                        command[interactionName](interaction, this)
+                    else 
+                        await error()
+                }
             }
         })
 
@@ -84,7 +125,21 @@ export interface Machi {
     /** This is for adding in the command information */
     data: Partial<SlashCommandBuilder>,
     /** The function that should be called when activated */
-    execute(interaction: CommandInteraction): Promise<void>,
+    execute(interaction: CommandInteraction, bot?: Machina): Promise<void>,
+    // messageComponent?: {
+    /** Listen to a button interaction, with the key being name and value being the function to execute */
+    button?: {
+        [key: string]: (interaction: ButtonInteraction, bot?: Machina) => Promise<void>
+    },
+    /** Listen to a context menu interaction, with the key being name and value being the function to execute */
+    contextMenu?: {
+        [key: string]: (interaction: ContextMenuInteraction, bot?: Machina) => Promise<void>
+    },
+    /** Listen to a select menu component interaction, with the key being name and value being the function to execute */
+    selectMenu?: {
+        [key: string]: (interaction: SelectMenuInteraction, bot?: Machina) => Promise<void>
+    },
+    // }
     /** If the command should be sent to discord */
     inDev: boolean
 }
