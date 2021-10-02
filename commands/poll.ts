ba913faea943634c9015ecc50ef4bef22970c331
Hamziniii <hamzagaziuddin04@gmail.com>
2021-10-02 13:05:28 -0500

    umm a bunch of changes

--- commands/poll.ts
+++ commands/poll.ts
@@ -1,11 +1,18 @@
 import { SlashCommandBuilder } from "@discordjs/builders"
-import { CommandInteraction, MessageActionRow, MessageSelectMenu, SelectMenuInteraction } from "discord.js"
+import { CommandInteraction, MessageActionRow, MessageEmbed, MessageSelectMenu, SelectMenuInteraction } from "discord.js"
 import { Machi, Machina } from "../lib/machina"
 import { replyOrFollowup } from "../lib/util"
 
 
 export const poll: Machi = {
-    data: (new SlashCommandBuilder()).setDescription("Create A Poll!").addStringOption(option => option.setName('options').setDescription("Set your poll options, separated by a comma").setRequired(true)),
+    data: (new SlashCommandBuilder())
+        .setDescription("Create A Poll!")
+        .addStringOption(option => option.setName('title')
+            .setDescription("Set the title of your poll").setRequired(true))
+        .addStringOption(option => option.setName('options')
+            .setDescription("Set your poll options, separated by a comma").setRequired(true))
+        .addStringOption(option => option.setName('description')
+            .setDescription('Add a description to your poll').setRequired(false)),
     execute: async (interaction: CommandInteraction, bot: Machina) => {
         const options = interaction.options.getString('options').split(', ').join(',').split(',')
         const row = new MessageActionRow()
@@ -17,7 +24,11 @@ export const poll: Machi = {
             )        
 
         await interaction.reply({
-            content: `Poll by: ${interaction.user.username}\nEnter your responses below!`,
+            embeds: [new MessageEmbed({ 
+                title: `Polls - ${interaction.options.getString('title')}`,
+                description: interaction.options.getString('description') || "A poll!", 
+                footer: {text: `Poll started by ${interaction.user.username}`}
+            })],
             components: [row]
         })
 
@@ -25,13 +36,20 @@ export const poll: Machi = {
         
         setTimeout(((i: CommandInteraction) => {
             i.editReply({
-                content: "Poll has concluded!"
+                components: [],
+                embeds: [new MessageEmbed({
+                    title: `Poll - ${interaction.options.getString('title')} Concluded!`,
+                    description: interaction.options.getString('description') || "A poll!", 
+                    footer: {text: `Poll started by ${interaction.user.username}`}
+                })]
             })
         }).bind(null, interaction), 1000 * 10)
     }, 
     selectMenu: {
         select: async (interaction: SelectMenuInteraction, bot: Machina) => {
             // bot.client.commands.get('poll')
+            // bot.getCommandSelf(this).storage =
+            console.log("potato")
             interaction[replyOrFollowup(interaction)]({
                 content: `Received: ${interaction.values.join('\n')}`,
                 ephemeral: true
--- lib/machina.ts
+++ lib/machina.ts
@@ -6,8 +6,6 @@ import * as fs from 'fs'
 import { SlashCommandBuilder } from '@discordjs/builders'
 import { sleep } from './util'
 import path from 'path'
-import * as ts from 'typescript'
-import requireFromString from 'require-from-string'
 
 /** Main class that handles command updates and starting the bot */
 export class Machina {
@@ -19,7 +17,7 @@ export class Machina {
     loggedIn: boolean 
 
     /**
-     * 
+     * Makes an instance of the Machina class
      * @param TOKEN Token of the bot
      * @param client_id ID of the bot
      * @param guild_id The SINGLE guild you want your commands to go under
@@ -34,66 +32,65 @@ export class Machina {
 
     /** Starts the bot */
     start() {
-        if(!this.client.commands)
-            this.reloadCommands()
+        if(!this.client.commands) // Checks to see if the commands have already been added
+            this.reloadCommands() // If not, then reload (technically load in this case) the commands
 
-        this.client.once('ready', () => {
-            console.log('Bot Online!')
+        this.client.once('ready', () => { // Once the bot has connected to discord
+            console.log('Bot Online!') // log that the bot is online
         })
-        this.client.on('interactionCreate', async interaction => {
-            if (interaction.isCommand()) {
-                const command = this.client.commands.get(interaction.commandName)
+        this.client.on('interactionCreate', async interaction => { // Listen to when a user either runs a command, or responds to a command 
+            if (interaction.isCommand()) { // If its a command 
+                const command = this.client.commands.get(interaction.commandName) // Check to see if the command they ran corresponds to a command in the cache
+                if (!command) return // If not, return 
             
-                if (!command) return
-            
-                try {
-                    await command.execute(interaction, this)
+                try { 
+                    await command.execute(interaction, this) // Try to run the command 
                 } catch (error) {
-                    console.error(error)
-                    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+                    console.error(error) // If there is an error, log it out
+                    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true }) // Reply to the user saying that there was an error
                 }
-            } else if(interaction.isMessageComponent()) {
-                // let type: 'button' | 'contextMenu' | 'selectMenu'
-                let [commandName, interactionName] = interaction.customId.split('.')
-                let command 
+            } else if(interaction.isMessageComponent()) { // Check to see if the interaction is a message component
+                let [commandName, interactionName] = interaction.customId.split('.') // Separate the command name and the interaction name
+                let command
 
                 const error = () => interaction.reply({ content: 'Uh Oh! The devs made a mistake while creating this command.', ephemeral: true})
 
-                if(interaction.isButton()) {
-                    command = this.client.commands.get(commandName)['button']
-                    if(command[interactionName])
-                        command[interactionName](interaction, this)
+                if(interaction.isButton()) { // Check to see if its a button action
+                    command = this.client.commands.get(commandName)['button'] // Get button handlers from the command (if there is one that is)
+                    if(command[interactionName]) // If the particular button interaction exists
+                        command[interactionName](interaction, this) // THen run it!
                     else 
-                        await error()
-                } else if(interaction.isContextMenu()) {
-                    command = this.client.commands.get(commandName)['contextMenu']
-                    if(command[interactionName])
-                        command[interactionName](interaction, this)
+                        await error() // Else error
+                } else if(interaction.isContextMenu()) { // Check to see if its a context menu action
+                    command = this.client.commands.get(commandName)['contextMenu'] // Get context handlers from the command (if there is one that is)
+                    if(command[interactionName]) // If the particular context menu interaction exists 
+                        command[interactionName](interaction, this) // Then run it!
                     else 
-                        await error()
-                } else if(interaction.isSelectMenu()) {
-                    command = this.client.commands.get(commandName)['selectMenu']
-                    if(command[interactionName])
-                        command[interactionName](interaction, this)
+                        await error() // Else error
+                } else if(interaction.isSelectMenu()) { // Check to see if its a select menu action
+                    command = this.client.commands.get(commandName)['selectMenu'] // Get select menu handler form the command (if there is one that is)
+                    if(command[interactionName]) // If the particular select menu interaction exists
+                        command[interactionName](interaction, this) // Then run it!
                     else 
-                        await error()
+                        await error() // Else error 
                 }
             }
         })
 
-        this.login()
+        this.login() // Logs the bot in!
     }
 
     /** Logs in the bot ONLY */
     async login() {
-        if(!this.loggedIn) {
-            await this.client.login(this.token)
-            this.loggedIn = true
+        if(!this.loggedIn) { // Check to see if the bot is already logged in
+            await this.client.login(this.token) // Wait for the bot to log in 
+            this.loggedIn = true // Set logged in to true
         }
     }
 
     /** Reloads or Uploads the commands in the bot that are in the commands folder */
     reloadCommands() {
+<<<<<<< Updated upstream
         if(this.client.commands)
             delete this.client.commands
         console.log(this.client.commands)
@@ -119,6 +116,26 @@ export class Machina {
                 }
             } catch (error) {
                 console.error(`UH OH, error loading ${file}\n Error: ${error}`)
+=======
+        if(this.client.commands) // Check to see if the command cache exists
+            delete this.client.commands // If so, delete it
+        this.client.commands = new Collection() // Create a new cache
+        for (const file of fs.readdirSync(path.join(__dirname, '../commands')).filter(file => file.endsWith('.js') || file.endsWith('.ts'))) { // Get each file from the commands folder
+            try {
+                console.log(file) // Log out the file name
+                delete require.cache[require.resolve(`../commands/${file}`)] // Remove the command from the program cache 
+                let _command = require(`../commands/${file}`) // Get the contents from the file 
+                const name = Object.getOwnPropertyNames(_command)[1] // Get the name of the command
+                let command: Machi = _command[name] // Get the actual command from the file
+                if(!command.inDev && command.data && command.execute) { // Check to see if the command should be added (not in dev), if the command has data, and if the command has an execute function
+                    if(command.data.name == undefined) // If the name is not set
+                        command.data.setName(name) // Then set the name 
+                    this.client.commands.set(command.data.name, command) // Add the command to the cache
+                    console.log(command.data.name, "has been added!") // Log that it has been added
+                }
+            } catch (error) {
+                console.error(`UH OH, error loading ${file}\n Error: ${error}`) // If there is an error somewhere, log it out 
+>>>>>>> Stashed changes
             }
         }
     }
@@ -126,24 +143,24 @@ export class Machina {
     // TODO: Add ability to have commands in subfolders 1 level deep only and error out on naming conflicts
     /** Takes all the files in commands folder, and uploades them to discord (except those with inDev == true)  */
     async updateCommands() {
-        this.reloadCommands()
+        this.reloadCommands() // Reloads the commands 
 
-        if([...this.client.commands.values()].length < 1)
-            return 
+        if([...this.client.commands.values()].length < 1) // Check to see if there are any commands in the cache 
+            return // If none, return 
 
-        console.log(`=======\nUpdating list of commands in five seconds`)
+        console.log(`=======\nUpdating list of commands in five seconds`) // Create a countdown and wait for five seconds. This is because we don't want to spam discord's API so you have time to stop it.
         for await (let k of (new Array(5)).fill(0).map((v, i) => 5 - i))
             await [console.log(`In ${k}...`), sleep(1000)][1]
 
-        await this.rest.put(Routes.applicationGuildCommands(this.client_id, this.guild_id), { body: this.client.commands.mapValues((v) => v.data.toJSON()) })
+        await this.rest.put(Routes.applicationGuildCommands(this.client_id, this.guild_id), { body: this.client.commands.mapValues((v) => v.data.toJSON()) }) // Sends the command infromation to discord 
             .then(() => console.log("Successfully Updated!"))
             .catch(e => console.error("Looks like there was an error!", e))
 
-        await this.login()
+        await this.login() // Log in. This is neccessary because we need to know specific information about the command to add in permissions to that command 
 
-        let commandNameAndIdsObject: { [name: string]: ApplicationCommand } = {} // {name: _.name, id: _.id}
-        ;(await this.client.guilds.cache.get(this.guild_id)?.commands.fetch()).toJSON().forEach(_ => commandNameAndIdsObject[_.name] = _)
-        this.client.commands.filter(command => (command?.permissions ?? 0) != 0).toJSON().forEach(_ => commandNameAndIdsObject[_.data.name].permissions.set({permissions: _.permissions}))
+        let commandNameAndIdsObject: { [name: string]: ApplicationCommand } = {} // Create an object to hold the data
+        ;(await this.client.guilds.cache.get(this.guild_id)?.commands.fetch()).toJSON().forEach(_ => commandNameAndIdsObject[_.name] = _) // For each command in the guild, get its data
+        this.client.commands.filter(command => (command?.permissions ?? 0) != 0).toJSON().forEach(_ => commandNameAndIdsObject[_.data.name].permissions.set({permissions: _.permissions})) // For each command in the cache that has permissions, set the corresponding command in the guild its permissions
     }
 
 
@@ -190,7 +207,7 @@ export interface Machi {
  * TODO
  * [x] figure the guild fetch commands
  * [x] add it so permissions update
- * [x] test out reload command
+ * [ ] test out reload command
  * [ ] finish poll
  * [ ] start on new command
  */ 
