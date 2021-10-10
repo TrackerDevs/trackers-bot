dd118dacf490c856ced9e427602e8ac4369f936c
Hamziniii <hamzagaziuddin04@gmail.com>
2021-10-10 17:44:27 -0500

    Birthday add complete

--- commands/birthday.ts
+++ commands/birthday.ts
@@ -1,14 +1,14 @@
 import { SlashCommandBuilder } from "@discordjs/builders"
 import { CommandInteraction } from "discord.js"
-import { Machi } from "../lib/machina"
-
+import { Machi, Machina, MachiUtil } from "../lib/machina"
+import { UserModel } from "../lib/mongo"
 
 export const birthday: Machi = {
     data: (new SlashCommandBuilder()).setDescription("Command for birthday related stuff")
       .addSubcommand(command => command.setName("add").setDescription("Add/Edit your birthday to the database!")
-        .addStringOption(sOp => sOp.setName("month").setDescription("The month you were born in. Ex: February").addChoices([["January", "01"], ["February", "02"], ["March", "03"], ["April", "04"], ["May", "05"], ["June", "06"], ["July", "07"], ["August", "08"], ["September", "09"], ["October", "10"], ["November", "11"], ["December", "12"]]).setRequired(true))
-        .addNumberOption(nOp => nOp.setName("day").setDescription("The day you were born. Ex: 04").setRequired(true))
-        .addNumberOption(nOp => nOp.setName("year").setDescription("The day you were born. Ex: 2000").setRequired(true)))
+        .addStringOption(sOp => sOp.setName("month").setDescription("The month you were born in. Ex: February").addChoices([["January", "January"], ["February", "February"], ["March", "March"], ["April", "April"], ["May", "May"], ["June", "June"], ["July", "July"], ["August", "August"], ["September", "September"], ["October", "October"], ["November", "November"], ["December", "December"]]).setRequired(true))
+        .addIntegerOption(nOp => nOp.setName("day").setDescription("The day you were born. Ex: 04").setRequired(true))
+        .addIntegerOption(nOp => nOp.setName("year").setDescription("The day you were born. Ex: 2000").setRequired(true)))
       .addSubcommand(command => command.setName("remove").setDescription("Remove your birthday from the database")
         .addBooleanOption(bOp => bOp.setName("confirm").setDescription("Are you sure you would like to delete your birthday from the database").setRequired(true)))
       .addSubcommand(command => command.setName("get").setDescription("Get the birthday of a user!")
@@ -16,17 +16,42 @@ export const birthday: Machi = {
       ),
     execute: async (interaction: CommandInteraction) => {
       interaction.reply({content: interaction.options.getSubcommand() + " recieved", ephemeral: true})
-      
-      switch(interaction.options.getSubcommand()) {
-        case "add":
-          break;
-        case "remove":
-          break;
-        case "edit":
-          break;
-        case "get":
-          break;
-      }
+    },
+    subCommands: {
+      add: async (interaction: CommandInteraction, bot: Machina) => {
+        UserModel.findOneAndUpdate({
+          id: interaction.user.id
+        }, {
+          birthday: {
+            month: interaction.options.getString("month"), 
+            day: interaction.options.getInteger("day"),
+            year: interaction.options.getInteger("year")
+          }
+        }, {upsert: true})
+          .then(() => {
+            interaction[MachiUtil.replyOrFollowup(interaction)]({
+              embeds: [{
+                author: {
+                  name: interaction.user.username,
+                  icon_url: interaction.user.avatarURL()
+                },
+                title: "Birthday Added!",
+                description: "Your birthday has been added to the database.",
+                fields: [
+                  {name: "Month", value: interaction.options.getString("month"), inline: true},
+                  {name: "Day", value: "" + interaction.options.getInteger("day"), inline: true},
+                  {name: "Year", value: "" + interaction.options.getInteger("year"), inline: true}
+                ]
+              }]
+            }) 
+          })
+          .catch(console.error)
+      },
+      remove: async (interaction: CommandInteraction, bot: Machina) => {},
+      get: async (interaction: CommandInteraction, bot: Machina) => {
+        interaction.reply("potato")
+        UserModel.find().exec().then(console.log)
+      },
     },
     inDev: false 
 }
