4c40bc04e5743a8533dc3544e9e8b3fa54161ed5
Hamziniii <hamzagaziuddin04@gmail.com>
2021-12-24 14:29:56 -0600

    added timedDestroy

--- lib/util.ts
+++ lib/util.ts
@@ -1,2 +1,5 @@
 /** Returns a promise that waits for (ms) milliseconds before resolving */
 export const sleep = async (ms: number): Promise<void> => new Promise(resolve => setTimeout(resolve, ms))
+
+/** Deletes a property off of an objects after a certain amount of time */
+export const timedDestroy = async (val: object, key: string, ms: number) =>  [await sleep(ms), delete val[key]]
