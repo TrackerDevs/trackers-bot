61b08f8cef17ab4ef2ee7e22ede7c08e77c1ca11
Hamziniii <hamzagaziuddin04@gmail.com>
2022-01-01 23:30:52 -0600

    fixed name clash with process.env.USERNAME w/ mongo

--- main.ts
+++ main.ts
@@ -6,9 +6,9 @@ require('dotenv').config() // This is for the token in the .env file
 // Your token, client id of the bot, guild id of where you want the commands to be, and any extra permissions
 const bot = new Machina(process.env['TOKEN'], process.env['CLIENT_ID'], process.env['GUILD_ID'], Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS)
 
-// This is a self calling function: it waits for the commands to updates, then starts the bot
+// This is a self calling function (aka IIFE): it waits for the commands to updates, then starts the bot
 ;(async (b) => {
-    await mongoose.connect(`mongodb+srv://${process.env['USERNAME']}:${process.env['PASSWORD']}` + process.env['URL'], {
+    await mongoose.connect(`mongodb+srv://${process.env['_USERNAME']}:${process.env['_PASSWORD']}` + process.env['URL'], {
         autoIndex: false,
         retryWrites: false,
         keepAlive: true
@@ -16,6 +16,6 @@ const bot = new Machina(process.env['TOKEN'], process.env['CLIENT_ID'], process.
     .then(() => console.log("Mongo connected!"))
     .catch(r => console.error("Error connecting to mongo! " + r))
 
-    // await b.updateCommands() // This is to update the command declerations, you can comment it out if you know that you aren't updating the `data` of any command
+    await b.updateCommands() // This is to update the command declerations, you can comment it out if you know that you aren't updating the `data` of any command
     b.start()
 })(bot)  
