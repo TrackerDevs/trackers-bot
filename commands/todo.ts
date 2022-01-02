560acb2804f46379275d53ca0f1ac336c821678a
Hamziniii <hamzagaziuddin04@gmail.com>
2022-01-01 23:17:46 -0600

    placeholder for todo

--- commands/todo.ts
+++ commands/todo.ts
@@ -1,12 +1,13 @@
 import { SlashCommandBuilder } from "@discordjs/builders"
 import { CommandInteraction } from "discord.js"
-import { Machi } from "../lib/machina"
+import { Machi, MachiUtil } from "../lib/machina"
 
-
-export const _: Machi = {
-    data: (new SlashCommandBuilder()).setDescription(""),
+export const todo: Machi = {
+    data: (new SlashCommandBuilder()).setDescription("The command for all your todo needs!"),
     execute: async (interaction: CommandInteraction) => {
-
+        interaction[MachiUtil.replyOrFollowup(interaction)]({
+            content: "TODO is being implemented! Please be patient :)\n - ProjectBot Devs"
+        })
     },
     inDev: true 
 }
