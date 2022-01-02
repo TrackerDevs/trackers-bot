fb140b27021f3a99f8aef6918b9d17879fe881cf
Hamziniii <hamzagaziuddin04@gmail.com>
2022-01-02 00:15:16 -0600

    extra util

--- lib/util.ts
+++ lib/util.ts
@@ -3,3 +3,9 @@ export const sleep = async (ms: number): Promise<void> => new Promise(resolve =>
 
 /** Deletes a property off of an objects after a certain amount of time */
 export const timedDestroy = async (val: object, key: string, ms: number) =>  [await sleep(ms), delete val[key]]
+
+/** If the input is not an array, turns it into a singleton array */
+export const arrify = <T>(item: T) => item instanceof Array ? item : [item]
+
+/** NoOp as in No Operation */
+export const noop = () => {}
