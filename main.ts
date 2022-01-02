049dffd7918cead7ccfc3e4a27dae05c8bd6190a
Hamziniii <hamzagaziuddin04@gmail.com>
2022-01-01 23:12:21 -0600

    possibly fix env?

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 import { Intents } from 'discord.js'
 import mongoose from 'mongoose'
 import { Machina } from './lib/machina'
-const env = require('dotenv').config().parsed // This is for the token in the .env file 
-console.log(env)
+require('dotenv').config() // This is for the token in the .env file 
+console.log(process.env)
 
 // Your token, client id of the bot, guild id of where you want the commands to be, and any extra permissions
-const bot = new Machina(env['TOKEN'], env['CLIENT_ID'], env['GUILD_ID'], Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS)
+const bot = new Machina(process.env['TOKEN'], process.env['CLIENT_ID'], process.env['GUILD_ID'], Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS)
 
 // This is a self calling function: it waits for the commands to updates, then starts the bot
 ;(async (b) => {
-    await mongoose.connect(`mongodb+srv://${env['USERNAME']}:${env['PASSWORD']}` + env['URL'], {
+    await mongoose.connect(`mongodb+srv://${process.env['USERNAME']}:${process.env['PASSWORD']}` + process.env['URL'], {
         autoIndex: false,
         retryWrites: false,
         keepAlive: true
