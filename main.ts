7b981ca1c2fd5c9c643ee1cf09b856badc95dd9d
Hamziniii <hamzagaziuddin04@gmail.com>
2022-01-01 23:09:14 -0600

    trying to figure out env issue

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import { Intents } from 'discord.js'
 import mongoose from 'mongoose'
 import { Machina } from './lib/machina'
 const env = require('dotenv').config().parsed // This is for the token in the .env file 
+console.log(env)
 
 // Your token, client id of the bot, guild id of where you want the commands to be, and any extra permissions
 const bot = new Machina(env['TOKEN'], env['CLIENT_ID'], env['GUILD_ID'], Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS)
@@ -16,6 +17,6 @@ const bot = new Machina(env['TOKEN'], env['CLIENT_ID'], env['GUILD_ID'], Intents
     .then(() => console.log("Mongo connected!"))
     .catch(r => console.error("Error connecting to mongo! " + r))
 
-    await b.updateCommands() // This is to update the command declerations, you can comment it out if you know that you aren't updating the `data` of any command
+    // await b.updateCommands() // This is to update the command declerations, you can comment it out if you know that you aren't updating the `data` of any command
     b.start()
 })(bot)  
