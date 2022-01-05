c9d9746d3848fc4e7a4679fba05b54958a8a613a
Hamziniii <hamzagaziuddin04@gmail.com>
2022-01-04 18:29:45 -0600

    basis for the todo command

--- commands/todo.ts
+++ commands/todo.ts
@@ -4,7 +4,13 @@ import { Machi, MachiUtil } from "../lib/machina"
 
 export const todo: Machi = {
     data: (new SlashCommandBuilder()).setDescription("The command for all your todo needs!")
-    .addSubcommand(sC => sC.setName("list").setDescription("List all your todos!")),
+    .addSubcommand(sC => sC.setName("list").setDescription("List all your todos!"))
+    .addSubcommand(sC => sC.setName("add").setDescription("Add a todo to your list!")
+        .addStringOption(sO => sO.setName("name").setDescription("The name of your todo").setRequired(true))
+        .addStringOption(sO => sO.setName("description").setDescription("The description of your todo").setRequired(false))
+        .addStringOption(sO => sO.setName("date").setDescription("The due date of your todo in (mm/dd/yyyy format). If no time is specified, the time will be assumed to be 12:00am.").setRequired(false))
+        .addStringOption(sO => sO.setName("time").setDescription("The time that the todo is due. If no date is specified, the date is assumed to be today."))
+    ),
     execute: async (interaction: CommandInteraction) => {
         interaction[MachiUtil.replyOrFollowup(interaction)]({
             content: "TODO is being implemented! Please be patient :)\n - ProjectBot Devs",
@@ -12,9 +18,13 @@ export const todo: Machi = {
         })
     },
     subCommands: {
-        "list": async (interaction, bot, uuid) => {
+        "list": async (interaction, bot) => {
+            
+        }, 
+        "add": async (interaction, bot) => {
 
         }
     },
-    inDev: false 
+    upload: 1, 
+
 }
